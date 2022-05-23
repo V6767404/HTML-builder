85639f3c2fbefcf86d00726824470707723174c6
Victor Y <v6767404@gmail.com>
2022-05-24 01:11:13 +0300

    feat: add task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+const curPath = path.join(__dirname, "files");
+const targetPath = path.join(__dirname, "files-copy");
 
+const copyDir = () => {
+  fs.mkdir(targetPath, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(curPath, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      fs.copyFile(path.join(curPath, file.name), path.join(targetPath, file.name), (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+};
+
+copyDir();
